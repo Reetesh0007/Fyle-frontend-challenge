29f0d8f6c62389841c221f3d809b4893cc78b73d
Reetesh0007 <hrieteshaarya2428@gmail.com>
2024-05-26 11:41:06 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HttpClientModule } from  '@angular/common/http';
+import { UserFormComponent } from './user-form/user-form.component';
+import { RepoListComponent } from './repo-list/repo-list.component';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { RepositoryComponent } from './repository/repository.component';
+import {NgxPaginationModule} from 'ngx-pagination';
+import { SpinnerComponent } from './spinner/spinner.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MyNewComponentComponent } from './my-new-component/my-new-component.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserFormComponent,
+    RepoListComponent,
+    RepositoryComponent,
+    SpinnerComponent,
+    MyNewComponentComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    AppRoutingModule,
+    NgxPaginationModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
